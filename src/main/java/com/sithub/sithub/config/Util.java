2149c19ffcf2773ade0f02aae2a80b9a8b7752d2
gyumin <gamgam0330@naver.com>
2024-03-19 15:10:11 +0900

    feat : 토큰 기능 추가

---
+++ src/main/java/com/sithub/sithub/config/Util.java
@@ -0,0 +1,47 @@
+package com.sithub.sithub.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class Util {
+    //Token에서 UserId값 찾기
+    public static Long getUserId(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("id", Long.class);
+    }
+
+    //Token에서 Nickname 찾기
+    public static String getNickname(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("nickname", String.class);
+    }
+
+
+    public static boolean isExpired(String token, String secretKey) {
+        // new Date() (현재시각) 기준으로 전이면 True, 아니면 false 라는 의미인듯 싶다
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date());
+    }
+
+    private static final long expireMs = 30 * 24 * 60 * 60 * 1000L; // 토큰 만료 시간 (한 달)
+
+    //아이디와 닉네임 가지고 토큰 생성.
+    public static String createJwt(Long id, String nickname, String secretKey) {
+        Claims claims = Jwts.claims();
+        claims.put("id", id);
+        claims.put("nickname", nickname);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expireMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+}
---
+++ src/main/java/com/sithub/sithub/domain/User.java
@@ -0,0 +1,22 @@
+package com.sithub.sithub.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private String password;
+}
+
