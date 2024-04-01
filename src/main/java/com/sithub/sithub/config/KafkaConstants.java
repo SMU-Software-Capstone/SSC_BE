56714e80eb1fba8a56cd6e166f751404522b6969
gyumin <gamgam0330@naver.com>
2024-04-01 16:17:01 +0900

    modify : 컨슈머 설정 group Id 랜덤값 수정

--- src/main/java/com/sithub/sithub/config/KafkaConstants.java
+++ src/main/java/com/sithub/sithub/config/KafkaConstants.java
@@ -1,7 +1,9 @@
 package com.sithub.sithub.config;
 
+import java.util.UUID;
+
 public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-chat";
-    public static final String GROUP_ID = "foo";
+    public static final String GROUP_ID = UUID.randomUUID().toString();
     public static final String KAFKA_BROKER = "kafka:9092";
 }
