93b35bf41e19e99094ac40e05aee22927a0dc361
gyumin <gamgam0330@naver.com>
2024-04-15 16:49:33 +0900

    feat : code 2차원 배열 수정

---
+++ src/main/java/com/sithub/sithub/requestDTO/CodeDTO.java
@@ -0,0 +1,11 @@
+package com.sithub.sithub.requestDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CodeDTO {
+    private String code;
+    private int lineNumber;
+}
