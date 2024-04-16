ed1f4eb16fc4aa5d105e294f46cd95965475dd11
bak3839 <bak3839@gmail.com>
2024-04-16 16:57:04 +0900

    Feat: 프론트에서 파일 받아서 읽는 기능 구현

--- src/main/java/com/sithub/sithub/controller/SnapshotController.java
+++ src/main/java/com/sithub/sithub/controller/SnapshotController.java
@@ -10,7 +10,9 @@ import com.sithub.sithub.responseDTO.SnapshotDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -38,5 +40,13 @@ public class SnapshotController {
     public void updateCode(@PathVariable("roomId") String roomId, @RequestBody CodeDTO code) {
         snapshotService.updateCodes(roomId, code.getCode(), code.getLineNumber());
         //mariaService.updateCodes(roomId, code.getCode());
+    }
+
+    //새 프로젝트 파일 받기
+    @PostMapping("/{teamId}/snapshots/upload")
+    public void upload(
+            @RequestPart(value = "files") List<MultipartFile> files,
+            @PathVariable("teamId") Long teamId) throws IOException {
+        snapshotService.uploadFile(files, teamId);
     }
 }
