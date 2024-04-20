f1892f40ae57aaa0e593ccf7e26a5e8e506b933a
bak3839 <bak3839@naver.com>
2024-04-20 19:07:33 +0900

    Feat: 팀별 형상관리 목록 반환하는 기능 구현

--- src/main/java/com/sithub/sithub/Repository/ManageRepository.java
+++ src/main/java/com/sithub/sithub/Repository/ManageRepository.java
@@ -1,7 +1,17 @@
 package com.sithub.sithub.Repository;
 
 import com.sithub.sithub.domain.Manage;
+import com.sithub.sithub.responseDTO.ManageListDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ManageRepository extends JpaRepository<Manage, Long> {
+
+    @Query("select m from Manage m " +
+            "where m.team.id = :teamId " +
+            "order by m.createDate ")
+    List<Manage> findManagesByTeamId(@Param("teamId") Long teamId);
 }
---
+++ src/main/java/com/sithub/sithub/controller/ManageController.java
@@ -0,0 +1,23 @@
+package com.sithub.sithub.controller;
+
+import com.sithub.sithub.Service.ManageService;
+import com.sithub.sithub.responseDTO.ManageListDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/manage")
+@RequiredArgsConstructor
+public class ManageController {
+    private final ManageService manageService;
+
+    @GetMapping("/list/{teamName}")
+    public List<ManageListDTO> list(@PathVariable("teamName") String teamName) {
+        return manageService.getManageList(teamName);
+    }
+}
