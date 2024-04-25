9e2eac876082b9526b83783547c9f81c4996a525
bak3839 <bak3839@naver.com>
2024-04-25 15:03:19 +0900

    Feat: 팀 소속 사용자 조회 기능 구현

--- src/main/java/com/sithub/sithub/controller/TeamController.java
+++ src/main/java/com/sithub/sithub/controller/TeamController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/team")
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ public class TeamController {
 
     private final TeamService teamService;
 
+    // 팀 생성
     @PostMapping("/create/{teamName}")
     public String create(@CookieValue("token") String token,
                        @PathVariable("teamName") String teamName) {
@@ -25,9 +28,16 @@ public class TeamController {
         return "success";
     }
 
+    // 팀에 사용자 추가
     @PostMapping("/{teamName}/add/{nickname}")
     public String add(@PathVariable("teamName") String teamName,
                       @PathVariable("nickname") String nickname) {
         return teamService.addUser(teamName, nickname);
+    }
+
+    // 팀 소속 사용자 조회
+    @GetMapping("/{teamName}/users")
+    public List<String> users(@PathVariable("teamName") String teamName) {
+        return teamService.userList(teamName);
     }
 }
